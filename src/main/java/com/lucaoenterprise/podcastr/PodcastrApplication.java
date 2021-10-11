f0486c308a9fc697aca0baccea26331e4bc8d6a4
lucasew <lucas59356@gmail.com>
2021-10-11 12:30:30 -0300

    Genesis
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ src/main/java/com/lucaoenterprise/podcastr/PodcastrApplication.java
@@ -0,0 +1,13 @@
+package com.lucaoenterprise.podcastr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucaoenterprise/podcastr/PodcastrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaoenterprise.podcastr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
