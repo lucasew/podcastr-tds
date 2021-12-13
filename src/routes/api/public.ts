92fe30a9742bb08652bcde09d5da52176ef08736
lucasew <lucas59356@gmail.com>
2021-12-13 17:44:02 -0300

    streaming funcionando caralhooo
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/routes/api/public.ts
+++ src/routes/api/public.ts
@@ -4,6 +4,7 @@ import { getConnection } from "typeorm";
 import Returner from "../../helpers/Returner";
 import EpisodeModel from "../../models/Episode";
 import PodcastModel from "../../models/Podcast";
+import got from 'got';
 
 const router = express.Router()
 
@@ -21,6 +22,22 @@ router.get("/episode/:id", async (req, res) => {
         Returner.errorCode(404, "episode not found")
     }
     Returner.json(ret[0])
+})
+
+router.get("/episode/:id/listen", async (req, res) => {
+    const { error, value } = Joi.object({
+        id: Joi.number().integer().sign('positive')
+    }).unknown(true).validate(req.params)
+    if (error) {
+        Returner.badRequest(error.message)
+    }
+    const ret = await getConnection().getRepository(EpisodeModel).findByIds([value.id])
+    if (ret.length === 0) {
+        Returner.errorCode(404, "episode not found")
+    }
+    const pod = ret[0]
+    const mp3res = got.stream(pod.mp3url)
+    mp3res.pipe(res)
 })
 
 router.get("/podcast", async (req, res) => {
