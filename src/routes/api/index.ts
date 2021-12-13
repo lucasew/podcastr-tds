7f6d7115a07f0def45ce3cae0b2f2348833b9d58
lucasew <lucas59356@gmail.com>
2021-12-13 14:52:29 -0300

    fix some warnings
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -2,14 +2,14 @@ import express from 'express'
 import admin from './admin'
 import auth from './auth'
 import demo from './demo'
-import public from './public'
+import pub from './public'
 
 const router = express.Router()
 
 router.use('/admin', admin)
 router.use('/auth', auth)
 router.use('/demo', demo)
-router.use('/public', public)
+router.use('/public', pub)
 
 router.use('/hello', (req, res) => {
     res.json({
